00df622218b676536871520a63ce22a66c2f9391
Maksim <mksmvnv@icloud.com>
2025-09-23 19:53:00 +0300

    chore: add docstrings

--- src/config.py
+++ src/config.py
@@ -5,10 +5,14 @@ from pydantic import BaseModel
 
 
 class BotSettings(BaseModel):
+    """Bot settings."""
+
     token: str
 
 
 class DatabaseSettings(BaseModel):
+    """Database settings."""
+
     dialect: str
     driver: str
     user: str
@@ -19,20 +23,25 @@ class DatabaseSettings(BaseModel):
 
     @property
     def url(self) -> str:
+        """Database URL."""
         return f"{self.dialect}+{self.driver}://{self.user}:{self.password}@{self.host}:{self.port}/{self.name}"
 
 
 class Settings(BaseModel):
+    """Application settings."""
+
     bot: BotSettings
     db: DatabaseSettings
 
     @classmethod
     def from_yaml(cls, path: Path) -> "Settings":
+        """Parse settings from YAML file."""
         with open(path) as f:
             return cls(**yaml.safe_load(f))
 
 
 def get_settings() -> Settings:
+    """Get application settings."""
     return Settings.from_yaml(Path(__file__).parent.parent / ".config" / "config.yaml")
 
 
