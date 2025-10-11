ffd49681b80a27f5b7a26c4d6fa3c476e010c0c4
Maksim <mksmvnv@icloud.com>
2025-10-11 13:22:54 +0300

    chore: add bot message texts

---
+++ src/texts/messages.py
@@ -0,0 +1,8 @@
+WELCOME = """
+Hi! I'm LinguAI Pro, your personal language assistant.
+I can help you with learning new languages and improving your skills.
+"""
+
+ERROR = """
+Sorry! Something went wrong. Please try again later.
+"""
