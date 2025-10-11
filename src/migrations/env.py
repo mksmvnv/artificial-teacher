55e7945c6cb14c72ce26ece552ec39dce0580224
Maksim <mksmvnv@icloud.com>
2025-10-11 12:57:28 +0300

    chore: update imports

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -6,14 +6,14 @@ from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
-from src.config import settings
+from src.core.config import settings
 from src.models.base import Base
 from src.models.user import User  # noqa: F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", settings.db.url)
+config.set_main_option("sqlalchemy.url", settings.database.url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
