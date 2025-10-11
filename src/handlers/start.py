b34c8b2b4987ecc210de2f9340ec17addbd21f59
Maksim <mksmvnv@icloud.com>
2025-10-11 13:19:44 +0300

    feat: implement /start command with user registration

---
+++ src/handlers/start.py
@@ -0,0 +1,34 @@
+import logging
+
+from aiogram import Router
+from aiogram.filters import CommandStart
+from aiogram.types import Message
+from dishka.integrations.aiogram import FromDishka, inject
+
+from src.schemas.user import UserSchema
+from src.services.start import StartService
+from src.texts.messages import WELCOME
+
+router = Router()
+logger = logging.getLogger(__name__)
+
+
+@router.message(CommandStart())
+@inject
+async def start_handler(message: Message, start_service: FromDishka[StartService]) -> None:
+    """Handle /start command."""
+    if not message.from_user:
+        logger.warning("User not found in message: %s", message)
+        return None
+
+    # Create user object
+    user = UserSchema(
+        tg_id=message.from_user.id,
+        username=message.from_user.username,
+        first_name=message.from_user.first_name,
+        last_name=message.from_user.last_name,
+    )
+
+    await start_service.register_user(user)
+    logger.info("User %d started bot", user.tg_id)
+    await message.answer(WELCOME)
