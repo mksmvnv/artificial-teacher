9e807b62a2c7a426d562ab051df5406897f6edbc
Maksim <mksmvnv@icloud.com>
2025-10-14 22:18:39 +0300

    feat: implement language keyboard

---
+++ src/keyboards/language.py
@@ -0,0 +1,30 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+
+class Language:
+    """Languages."""
+
+    ENGLISH: str = "English 🇺🇸"
+
+    @staticmethod
+    def get_callback_data(lang_code: str) -> str:
+        """Get callback data for language."""
+        return f"lang_{lang_code}"
+
+
+def get_language_keyboard() -> InlineKeyboardMarkup:
+    """Get languages keyboard."""
+    builder = InlineKeyboardBuilder()
+
+    languages = [
+        (Language.ENGLISH, "en"),
+    ]
+
+    for text, lang_code in languages:
+        builder.add(
+            InlineKeyboardButton(text=text, callback_data=Language.get_callback_data(lang_code))
+        )
+
+    builder.adjust(1)
+    return builder.as_markup()
