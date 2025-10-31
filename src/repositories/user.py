0ec8098d99e5e354f864f0f56f84b37498a6665c
Maksim <mksmvnv@icloud.com>
2025-10-31 22:51:58 +0300

    chore: add repository protocol for dependency injection

--- src/repositories/user.py
+++ src/repositories/user.py
@@ -1,16 +1,54 @@
+from typing import Protocol
+
 from src.models.user import User
 from src.repositories.base import BaseRepository
 from src.schemas.user import UserSchema
 
 
-class UserRepository(BaseRepository[User, UserSchema]):
+class UserRepositoryProtocol(Protocol):
+    """Protocol for user repository."""
+
+    async def find_by_tg_id(self, tg_id: int) -> UserSchema | None:
+        """Find user by telegram id or none."""
+        ...
+
+    async def add_one(self, data: UserSchema) -> int:
+        """Set user language by telegram id."""
+        ...
+
+    async def set_user_language(self, language: str, tg_id: int) -> None:
+        """Set user language by telegram id."""
+        ...
+
+    async def get_user_language(self, tg_id: int) -> str | None:
+        """Get user language by telegram id or none."""
+        ...
+
+    async def set_user_cefr_level(self, cefr_level: str, tg_id: int) -> None:
+        """Set user language by telegram id."""
+        ...
+
+    async def get_user_cefr_level(self, tg_id: int) -> str | None:
+        """Get user language by telegram id or none."""
+        ...
+
+
+class UserRepository(BaseRepository[User, UserSchema], UserRepositoryProtocol):
     """User repository."""
 
     model = User
 
-    async def find_by_tg_id(self, tg_id: int) -> User | None:
+    async def find_by_tg_id(self, tg_id: int) -> UserSchema | None:
         """Find user by telegram id or none."""
-        return await self.find_one_or_none(tg_id=tg_id)
+        user = await self.find_one_or_none(tg_id=tg_id)
+        if user:
+            return UserSchema(
+                tg_id=user.tg_id,
+                username=user.username,
+                first_name=user.first_name,
+                last_name=user.last_name,
+            )
+        return None
 
     async def set_user_language(self, language: str, tg_id: int) -> None:
         """Set user language by telegram id."""
@@ -18,7 +56,7 @@ class UserRepository(BaseRepository[User, UserSchema]):
 
     async def get_user_language(self, tg_id: int) -> str | None:
         """Get user language by telegram id or none."""
-        user = await self.find_by_tg_id(tg_id)
+        user = await self.find_one_or_none(tg_id=tg_id)
         return user.language if user else None
 
     async def set_user_cefr_level(self, cefr_level: str, tg_id: int) -> None:
@@ -27,5 +65,5 @@ class UserRepository(BaseRepository[User, UserSchema]):
 
     async def get_user_cefr_level(self, tg_id: int) -> str | None:
         """Get user language by telegram id or none."""
-        user = await self.find_by_tg_id(tg_id)
+        user = await self.find_one_or_none(tg_id=tg_id)
         return user.cefr_level if user else None
