adcc2c72834782bcd7914c448d2efc1ee5ac10e5
Maksim <mksmvnv@icloud.com>
2025-10-31 14:18:07 +0300

    feat: add language fields

--- src/models/user.py
+++ src/models/user.py
@@ -12,3 +12,5 @@ class User(Base):
     first_name: Mapped[str | None] = mapped_column(String(length=64), nullable=True)
     last_name: Mapped[str | None] = mapped_column(String(length=64), nullable=True)
     is_admin: Mapped[bool] = mapped_column(Boolean, default=False)
+    language: Mapped[str | None] = mapped_column(String(length=2), nullable=True)
+    cefr_level: Mapped[str | None] = mapped_column(String(length=2), nullable=True)
