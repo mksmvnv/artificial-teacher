76f7f91035835f927745a83b36b9afa86fa18f4a
Maksim <mksmvnv@icloud.com>
2025-10-31 14:20:54 +0300

    feat: add new methods to manage user language data

--- src/repositories/user.py
+++ src/repositories/user.py
@@ -11,3 +11,21 @@ class UserRepository(BaseRepository[User, UserSchema]):
     async def find_by_tg_id(self, tg_id: int) -> User | None:
         """Find user by telegram id or none."""
         return await self.find_one_or_none(tg_id=tg_id)
+
+    async def set_user_language(self, language: str, tg_id: int) -> None:
+        """Set user language by telegram id."""
+        await self.update_one(field_name="language", update_data=language, tg_id=tg_id)
+
+    async def get_user_language(self, tg_id: int) -> str | None:
+        """Get user language by telegram id or none."""
+        user = await self.find_by_tg_id(tg_id)
+        return user.language if user else None
+
+    async def set_user_cefr_level(self, cefr_level: str, tg_id: int) -> None:
+        """Set user language by telegram id."""
+        await self.update_one(field_name="cefr_level", update_data=cefr_level, tg_id=tg_id)
+
+    async def get_user_cefr_level(self, tg_id: int) -> str | None:
+        """Get user language by telegram id or none."""
+        user = await self.find_by_tg_id(tg_id)
+        return user.cefr_level if user else None
