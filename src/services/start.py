a2946558e8b960ecd4d9c0a46cedcc5fd44e5e0d
Maksim <mksmvnv@icloud.com>
2025-11-08 14:38:16 +0300

    refactor: update func and var names

--- src/services/start.py
+++ src/services/start.py
@@ -14,10 +14,10 @@ class StartService:
 
     async def register_user(self, user: UserSchema) -> int:
         """Register user if not exists, return user id."""
-        if existing := await self._user_repository.find_by_tg_id(user.tg_id):
-            logger.info("User '%d' already exists", existing.tg_id)
-            return existing.tg_id
+        if existing_user := await self._user_repository.find_by_user_tg_id(user.tg_id):
+            logger.info("User '%d' already exists", existing_user.tg_id)
+            return existing_user.tg_id
 
-        user_id = await self._user_repository.add_one(user)
+        user_id = await self._user_repository.add_new_user(user)
         logger.info("User '%d' created with id '%d'", user.tg_id, user_id)
         return user_id
