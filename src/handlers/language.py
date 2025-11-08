cd358ab6f900566e60730756ca19f4681426c7d0
Maksim <mksmvnv@icloud.com>
2025-11-08 14:30:25 +0300

    refactor: update var names and structure

--- src/handlers/language.py
+++ src/handlers/language.py
@@ -13,7 +13,7 @@ router = Router()
 logger = logging.getLogger(__name__)
 
 
-@router.callback_query(F.data.startswith("LANG_"))
+@router.callback_query(F.data.startswith("lang_"))
 @inject
 async def handle_language_selection(
     callback: CallbackQuery,
@@ -22,32 +22,33 @@ async def handle_language_selection(
 ) -> None:
     """Handle language selection."""
     callback_data = callback.data
-    tg_id = callback.from_user.id
+    user_tg_id = callback.from_user.id
 
     if not isinstance(callback_data, str):
         logger.warning("Callback data '%s' is not a string", callback_data)
-        return None
-
-    if not callback.message or not hasattr(callback.message, "edit_text"):
-        logger.warning("Message '%s' not accessible for editing", callback)
         return None
 
     language = callback_data.split("_")[1]
 
     # Set language in context and database
     await gather(
-        language_service.set_user_language(tg_id=tg_id, language=language),
-        context_service.set_user_language(tg_id=tg_id, language=language),
+        language_service.set_user_language(user_tg_id=user_tg_id, language=language),
+        context_service.set_context(prefix="user_language", user_tg_id=user_tg_id, data=language),
     )
 
     await callback.answer()
 
+    if not callback.message or not hasattr(callback.message, "edit_text"):
+        logger.warning("Message '%s' not accessible for editing", callback)
+        return None
+
     await callback.message.edit_text(
-        text=getattr(messages, f"LANG_{language.upper()}_START"), reply_markup=get_cefr_keyboard()
+        text=getattr(messages, f"{language.upper()}_LANGUAGE_START"),
+        reply_markup=get_cefr_keyboard(),
     )
 
 
-@router.callback_query(F.data.startswith("CEFR_"))
+@router.callback_query(F.data.startswith("cefr_level_"))
 @inject
 async def handle_cefr_level_selection(
     callback: CallbackQuery,
@@ -56,21 +57,29 @@ async def handle_cefr_level_selection(
 ) -> None:
     """Handle CEFR level selection."""
     callback_data = callback.data
-    tg_id = callback.from_user.id
+    user_tg_id = callback.from_user.id
 
     if not isinstance(callback_data, str):
         logger.warning("Callback data '%s' is not a string", callback_data)
         return None
 
     # Get language from context or database
-    language = await context_service.get_user_language(tg_id=tg_id)
+    language = await context_service.get_context(prefix="user_language", user_tg_id=user_tg_id)
 
     if not language:
-        logger.warning("Context does not contain language for user '%d'", tg_id)
-        language = await language_service.get_user_language(tg_id=tg_id)
+        language = await language_service.get_user_language(user_tg_id=user_tg_id)
         if not language:
-            logger.warning("User '%d' language not found in database", tg_id)
             return None
+
+    cerf_level = callback_data.split("_")[2]
+
+    # Set CEFR level in context and database
+    await gather(
+        language_service.set_user_cefr_level(user_tg_id=user_tg_id, cefr_level=cerf_level),
+        context_service.set_context(
+            prefix="user_cefr_level", user_tg_id=user_tg_id, data=cerf_level
+        ),
+    )
 
     await callback.answer()
 
@@ -78,10 +87,6 @@ async def handle_cefr_level_selection(
         logger.warning("Message '%s' not accessible for editing", callback)
         return None
 
-    cerf_level = callback_data.split("_")[1]
-
-    await language_service.set_user_cefr_level(tg_id=callback.from_user.id, cefr_level=cerf_level)
-
     await callback.message.edit_text(
-        text=getattr(messages, f"CEFR_{language.upper()}_START"), reply_markup=None
+        text=getattr(messages, f"{language.upper()}_CEFR_START"), reply_markup=None
     )
