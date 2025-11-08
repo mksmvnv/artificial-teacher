fb54a2aba2bd6fa48bc8433eedd132edabef1d2b
Maksim <mksmvnv@icloud.com>
2025-11-08 14:41:15 +0300

    chore: add assistant service

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -1,5 +1,6 @@
+from .assistant import AssistantService
 from .context import ContextService
 from .language import LanguageService
 from .start import StartService
 
-__all__ = ["StartService", "LanguageService", "ContextService"]
+__all__ = ["StartService", "LanguageService", "ContextService", "AssistantService"]
